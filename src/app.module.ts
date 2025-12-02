3bf5b914912f34d0873885120a6086d14cc4fc73
Matheus Jesus de Souza <offteuz@gmail.com>
2025-12-02 09:24:28 -0300

    chore: Importa o modulo de conexao do banco de dados a nivel da aplicacao

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    UsuarioModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
